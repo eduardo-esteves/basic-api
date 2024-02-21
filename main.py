15caa9324d60f52c443a1d407d578d6600f7c8da
eduardo-esteves <eduardostevesdasilva@hotmail.com>
2024-02-20 22:39:33 -0300

    Added a new courses endpoint with its data mocked

--- main.py
+++ main.py
@@ -2,10 +2,28 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+courses = {
+    1: {
+        "title": "PHP Advanced",
+        "lessons": 112,
+        "hours": 58,
+    },
+    2: {
+        "title": "Java How To Program",
+        "lessons": 87,
+        "hours": 67,
+    },
+}
+
 
 @app.get('/test')
 async def test():
     return {"msg": "Route ok"}
+
+
+@app.get('/courses')
+async def get_courses():
+    return courses
 
 
 if __name__ == '__main__':
